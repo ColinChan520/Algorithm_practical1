f0563f545ce6abfefd9232b4a4f78a35fb6ab0bd
ColinChan520 <a1864874@adelaide.edu.au>
2023-03-03 20:53:41 +1030

    Update Referee.cpp

--- Referee.cpp
+++ Referee.cpp
@@ -2,6 +2,11 @@
 #include "Referee.h"
 
     Player* Referee::refGame(Player*player1, Player*player2){
+        cout << player1->getMove() << endl;
+        cout << player2->getMove() << endl;
+        cout << player1->getName() << endl;
+        cout << player2->getName() << endl;
+
         if (player1->getMove() == 'S'){
             return player2;
         }
